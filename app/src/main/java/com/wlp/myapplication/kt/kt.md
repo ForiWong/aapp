88297e90a0ca109ce57e6baec18d0657e89a1353
wanglp2019 <xb80230218@163.com>
2022-06-09 18:16:29 +0800

    kt

--- app/src/main/java/com/wlp/myapplication/kt/kt.md
+++ app/src/main/java/com/wlp/myapplication/kt/kt.md
@@ -11,3 +11,16 @@
 对象表达式/声明
 委托
 
+（1）kotlin跳转到kotlin，或者kotlin跳转到java
+btn_interface.setOnClickListener {
+    var intent = Intent()
+    intent.setClass(this, MainActivity::class.java)
+    startActivity(intent)
+}
+
+（2）java跳转到kotlin中，和java跳转到java中是一样的
+Intent intent = new Intent(this, MainActivity.class);
+startActivity(intent);
+
+//交叉使用java、kotlin
+
--- app/src/main/java/com/wlp/myapplication/kt2/Lesson2.kt
+++ app/src/main/java/com/wlp/myapplication/kt2/Lesson2.kt
@@ -1,6 +1,7 @@
 package com.wlp.myapplication.kt2
 
 import java.lang.IllegalArgumentException
+import kotlin.math.roundToInt
 
 /**
  * Created by wlp on 2022/5/20
@@ -112,5 +113,122 @@ fun mainSplit(){
     val(v1, v2, v3, v4) = list
 }
 
+//replace
+fun main45(){
+    val srcPwd = "ABCDEFGHIJKLMNOPQ"
 
-//...45
+    val newPwd = srcPwd.replace(Regex("[AKM]")){
+        it.value //完全没有做任何操作
+
+        when(it.value){
+            "A" -> "9"
+            "M" -> "8"
+            "K" -> "7"
+            else -> it.value
+        }
+    }
+}
+
+//46. == 与 === 的区别
+// == 值、内容的比较 相当于java的equals
+// === 引用的比较
+fun main46(){
+    val name1 = "DERRY"
+    val name2 = "DERRY"
+    val name3 = "ww"
+
+    println(name1.equals(name2))//java
+    println(name1 == name2)//kt
+
+    println(name1 === name2)//true 常量池
+    println(name1 === name3)//false
+
+    val name4 = "derry".capitalize()
+    println(name1 === name4)//false
+}
+
+//47、字符串的遍历操作
+fun main47(){
+    val src = "ABCDEFGHIJKLMNOPQ"
+
+    src.forEach { c ->
+        //println("所有的字符是：$it ")
+        println("所有的字符是：$c ")
+    }
+}
+
+//48、数字类型的安全转换函数
+fun main48(){
+    val number : Int = "666".toInt()
+
+    val number2 : Int = "666.6".toInt()//崩溃
+
+    val number3: Int? = "666.6".toIntOrNull()
+
+    val number4: Int? = "888.8".toIntOrNull()
+    println(number4 ?: "原来你是null,转换错误了")
+}
+
+//49、Double转Int 与类型格式化
+fun main49(){
+    println(65.455.toInt()) //舍去小数
+    println(65.555.roundToInt()) //四舍五入
+
+    println("%.3f".format(65.555666))//格式化
+}
+
+//50、apply内置函数
+// 有点像链式编程
+fun main50(){
+    val info = "123dff"
+
+    info.apply {
+        println("长度是：$length")
+    }.apply {
+        println("${this[length - 1]}")
+    }.apply {
+        println("小写：${toLowerCase()}")
+    }
+}
+
+//51、let内置函数
+fun main53(){
+    val result2 = listOf(6,5,2,3,5,7).let {
+        //it == list集合
+        //匿名函数的最后一行，作为返回值。
+        it.first() + it.first()
+    }
+    println(result2)
+}
+
+fun getMethod2(value: String?) = if (value == null) "你是null" else "你是$value"
+
+//run
+//run 函数返回类型，是根据匿名函数最后一行返回的
+//run 函数的匿名函数持有的是this == str 本身
+fun main54(){
+    val str = "1234ppp"
+
+    str
+        .run(::isLong)
+        .run(::showText)
+        .run(::mapText)
+        .run(::println)
+
+    str.run{
+        this.length > 5
+    }.run{
+        if(this) "合格" else "不合格"
+    }.run{
+        "[$this]"
+    }.run{
+        println(this)
+    }
+}
+
+fun isLong(str: String) = str.length > 5
+
+fun showText(isLong: Boolean) = if (isLong) "合格" else "不合格"
+
+fun mapText(getShow: String) = "【$getShow】"
+
